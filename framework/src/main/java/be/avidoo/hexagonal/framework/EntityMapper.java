adb37e841e59c4062488215d639d64315ec4a45d
marcobrasci1986 <marco_brasci@hotmail.com>
2023-04-01 20:30:06 +0200

    Using Commands and DTO's

--- framework/src/main/java/be/avidoo/hexagonal/framework/EntityMapper.java
+++ framework/src/main/java/be/avidoo/hexagonal/framework/EntityMapper.java
@@ -1,12 +1,13 @@
 package be.avidoo.hexagonal.framework;
 
 import be.avidoo.hexagonal.domain.AggregateRoot;
+import be.avidoo.hexagonal.domain.EntityId;
 
 /**
  * D : DomainEntity
  * J: JpaEntity
  */
-public interface EntityMapper<D extends AggregateRoot, J> {
+public interface EntityMapper<D extends AggregateRoot<? extends EntityId<?>>, J> {
 
 
     J toJpaEntity(D domainEntity);
