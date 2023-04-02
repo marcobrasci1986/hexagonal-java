ad67c63290d7bf06d58642f86b8bf7bfb4338366
marcobrasci1986 <marco_brasci@hotmail.com>
2023-04-02 21:01:12 +0200

    Addind DomainEvent in ApplicationLayer

--- bootstrap/src/main/java/be/avidoo/hexagonal/Main.java
+++ bootstrap/src/main/java/be/avidoo/hexagonal/Main.java
@@ -1,14 +1,16 @@
 package be.avidoo.hexagonal;
 
+import be.avidoo.hexagonal.application.dossier.ApplicationConfiguration;
+import be.avidoo.hexagonal.framework.FrameworkConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 /**
  * We do componentScan in the current package and also in de ports package from the Application Hexagon to find the @Named services
  */
 @SpringBootApplication
-@ComponentScan(value = {"be.avidoo.hexagonal.framework", "be.avidoo.hexagonal.application"})
+@Import({ApplicationConfiguration.class, FrameworkConfiguration.class})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ framework/src/main/java/be/avidoo/hexagonal/framework/FrameworkConfiguration.java
@@ -0,0 +1,9 @@
+package be.avidoo.hexagonal.framework;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class FrameworkConfiguration {
+}
