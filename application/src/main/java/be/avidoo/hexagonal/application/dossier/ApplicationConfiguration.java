b33056ff427ac29cd82b9797c02c99b64f800029
marcobrasci1986 <marco_brasci@hotmail.com>
2023-04-02 21:02:12 +0200

    Addind DomainEvent in ApplicationLayer

--- application/src/main/java/be/avidoo/hexagonal/application/dossier/ApplicationConfiguration.java
+++ application/src/main/java/be/avidoo/hexagonal/application/dossier/ApplicationConfiguration.java
@@ -1,9 +1,17 @@
 package be.avidoo.hexagonal.application.dossier;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.Clock;
 
 @Configuration
 @ComponentScan
 public class ApplicationConfiguration {
+
+    @Bean
+    public Clock clock() {
+        return Clock.systemDefaultZone();
+    }
 }
